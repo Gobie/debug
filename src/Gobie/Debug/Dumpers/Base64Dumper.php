cb91a9894cb2fd64b55d1b1a0379469447d4e69b
Michal Brašna <michal.brasna@gmail.com>
2013-12-17 04:47:18 +0100

    Add Base64Dumper docs

--- src/Gobie/Debug/Dumpers/Base64Dumper.php
+++ src/Gobie/Debug/Dumpers/Base64Dumper.php
@@ -5,7 +5,9 @@ namespace Gobie\Debug\Dumpers;
 use Gobie\Debug\Helpers;
 
 /**
- * Dumper Base64.
+ * Base64 dumper.
+ *
+ * Expects utf-8 strings.
  */
 class Base64Dumper extends StringDumper
 {
@@ -23,11 +25,19 @@ class Base64Dumper extends StringDumper
                . $indentation . '<span class="dump_arg_expanded">' . $value . '</span>';
     }
 
+    /**
+     * {@inheritdoc}
+     * String must be non-empty.
+     * String must have length divisible by four.
+     * String must contain only of characters [a-zA-Z0-9/+=].
+     * String must after base64_decode contain only valid utf-8 characters.
+     * @link http://en.wikipedia.org/wiki/Base64
+     */
     public function canDump($var)
     {
         return $var
                && strlen($var) % 4 === 0
                && !preg_match('@[^a-zA-Z0-9/+=]@S', $var)
-               && preg_match('@\S@Su', base64_decode($var));
+               && preg_match('@@Su', base64_decode($var));
     }
 }
